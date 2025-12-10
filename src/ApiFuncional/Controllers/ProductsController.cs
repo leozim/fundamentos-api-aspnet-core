81c5fdb95df5f9ed748f097372db72e6c0d15b3a
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2025-12-10 16:36:10 -0300

    criando banco de dados e definindo ActionResults

---
+++ src/ApiFuncional/Controllers/ProductsController.cs
@@ -0,0 +1,68 @@
+﻿using ApiFuncional.Data;
+using ApiFuncional.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiFuncional.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductsController : ControllerBase
+{
+    private readonly ApiDbContext _context;
+
+    public ProductsController(ApiDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> Get()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Product>> Get(int id)
+    {
+        return await _context.Products.FindAsync(id);
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<Product>> Post(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("Get", new Product {Id = product.Id}, product);
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Put(int id, Product product)
+    {
+        var productToUpdate = await _context.Products.FindAsync(id);
+        if (productToUpdate?.Id != product.Id)
+        {
+            return NotFound();
+        }
+        
+        _context.Products.Update(product);
+        await _context.SaveChangesAsync();
+        
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var productToDelete = await _context.Products.FindAsync(id);
+        if (productToDelete == null)
+        {
+            return NotFound();
+        }
+        
+        _context.Products.Remove(productToDelete);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
---
+++ src/ApiFuncional/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFuncional.Models;
+
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    public string Name { get; set; }
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [Range(1, int.MaxValue, ErrorMessage = "O campo {0} deve ser maior ou igual a {1}.")]
+    public decimal Price { get; set; }
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    public int StockQuantity { get; set; }
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    public string Description { get; set; }
+}
