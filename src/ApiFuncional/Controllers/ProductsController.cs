e14ab0b5987fdf7cb9596296ba4a260b8a6c57d7
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2025-12-11 18:08:51 -0300

    usando Identity na minha aplicação

--- src/ApiFuncional/Controllers/ProductsController.cs
+++ src/ApiFuncional/Controllers/ProductsController.cs
@@ -17,20 +17,50 @@ public class ProductsController : ControllerBase
     }
 
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType]
     public async Task<ActionResult<IEnumerable<Product>>> Get()
     {
+        if (_context.Products == null) return NotFound();
+        
         return await _context.Products.ToListAsync();
     }
 
     [HttpGet("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesDefaultResponseType]
     public async Task<ActionResult<Product>> Get(int id)
     {
-        return await _context.Products.FindAsync(id);
+        if (_context.Products == null) return NotFound();
+        
+        var product = await _context.Products.FindAsync(id);
+        
+        return product == null ?  NotFound() :  product;
     }
     
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesDefaultResponseType]
     public async Task<ActionResult<Product>> Post(Product product)
     {
+        if (_context.Products == null) 
+            return Problem("Error ao criar o produto, contete o suporte!");
+        
+        if (!ModelState.IsValid)
+        {
+            // return BadRequest(ModelState);
+
+            // return ValidationProblem(ModelState);
+
+            return ValidationProblem(new ValidationProblemDetails(ModelState)
+            {
+                Title = "Um ou mais erros de validação ocorreram!",
+            });
+        }
         _context.Products.Add(product);
         await _context.SaveChangesAsync();
         
@@ -39,24 +69,58 @@ public class ProductsController : ControllerBase
     }
 
     [HttpPut("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesDefaultResponseType]
     public async Task<IActionResult> Put(int id, Product product)
     {
+        if (id != product.Id) return BadRequest();
+        
+        if (!ModelState.IsValid) return ValidationProblem(ModelState);
+        
+        // Abaixo minha validação
+        /*
         var productToUpdate = await _context.Products.FindAsync(id);
-        if (productToUpdate?.Id != product.Id)
+        if (productToUpdate == null)
         {
             return NotFound();
         }
+        */
+
+        _context.Entry(product).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!ProductExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
         
-        _context.Products.Update(product);
-        await _context.SaveChangesAsync();
+        // _context.Products.Update(product);
         
         return NoContent();
     }
 
     [HttpDelete("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesDefaultResponseType]
     public async Task<IActionResult> Delete(int id)
     {
+        if (_context.Products == null) return NotFound();
+        
         var productToDelete = await _context.Products.FindAsync(id);
+        
         if (productToDelete == null)
         {
             return NotFound();
@@ -65,5 +129,10 @@ public class ProductsController : ControllerBase
         _context.Products.Remove(productToDelete);
         await _context.SaveChangesAsync();
         return NoContent();
+    }
+
+    private bool ProductExists(int id)
+    {
+        return (_context.Products?.Any(e => e.Id == id)).GetValueOrDefault();
     }
 }
