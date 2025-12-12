0385e28d63b6f78b3051444432c7821c83dbcf64
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2025-12-12 10:42:56 -0300

    adicionando JWT no projeto

---
+++ src/ApiFuncional/Models/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiFuncional.Models;
+
+public class JwtSettings
+{
+    public string Secret { get; set; }
+    public int ExpirationHours { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+}
